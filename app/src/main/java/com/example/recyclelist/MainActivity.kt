02ed06292c8c0da8fe57bb818eedd8eb4327cfbd
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2021-09-23 19:57:12 -0600

    init

---
+++ app/src/main/java/com/example/recyclelist/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.recyclelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var kRecyclerView: RecyclerView
+    val koffeeList = arrayListOf<String>("Arabic", "Robusta","Sumatra","Kona")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        kRecyclerView =
+            findViewById(R.id.coffee_recycler_view) as RecyclerView
+        kRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        val kadapter: KAdapter  = KAdapter(this, koffeeList)
+
+        kRecyclerView.setAdapter(kadapter)
+    }
+}
