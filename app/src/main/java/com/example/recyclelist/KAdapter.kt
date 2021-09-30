3ff2cb8b7955aaa6df7b511f96e566b90ccc6cc2
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2021-09-30 17:19:36 -0600

    Midway point in Join

--- app/src/main/java/com/example/recyclelist/KAdapter.kt
+++ app/src/main/java/com/example/recyclelist/KAdapter.kt
@@ -10,25 +10,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 
-
-    public class KAdapter(context: Context, var coffee: ArrayList<String>)
+var combindedList = arrayListOf<String>()
+    class KAdapter(context: Context, var coffee: ArrayList<String>)
         : RecyclerView.Adapter<KAdapter.KoffeeHolder>() {
 
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
                 : KAdapter.KoffeeHolder {
-            val view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
 
             return KoffeeHolder(view, parent, coffee)
 
+
         }
 
-
+        fun getJoined(): ArrayList<String> = combindedList
 
         override fun getItemCount() = coffee.size
 
@@ -36,16 +35,28 @@ import androidx.recyclerview.widget.RecyclerView
             val acoffee = coffee[position]
             holder.apply {
                 titleTextView.text = acoffee
+
+                selected.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener() { buttonView, isChecked ->
+                    if(isChecked) {
+                        combindedList.add(coffee[position])
+                        Log.d("RecycleList",combindedList.toString())
+                    } else {
+                        combindedList.remove(coffee[position])
+                        Log.d("RecycleList",combindedList.toString())
+                    }
+                })
             }
 
         }
-        class KoffeeHolder(v: View, parent: ViewGroup, coffee: ArrayList<String>) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        class KoffeeHolder(v: View, parent: ViewGroup, coffee: ArrayList<String>) : RecyclerView.ViewHolder(v), View.OnClickListener{
             val titleTextView: TextView = v.findViewById(R.id.coffee_name)
             val alertDialog = AlertDialog.Builder(parent.context)
+            val selected: CheckBox =  v.findViewById(R.id.cbSelect)
             val coffee2 = coffee
-            init {
-                v.setOnClickListener(this)
 
+            init {
+
+                v.setOnClickListener(this)
 
             }
             override fun onClick(v: View?) {
@@ -55,6 +66,8 @@ import androidx.recyclerview.widget.RecyclerView
                     DialogInterface.OnClickListener { dialog, id ->
                        onRemove(titleTextView.text.toString())
                         Log.d("RecycleList",coffee2.toString())
+                        //super.getBindingAdapter()
+                        //v?.invalidate()
                     })
                 alertDialog.show()
 
@@ -63,8 +76,11 @@ import androidx.recyclerview.widget.RecyclerView
             fun onRemove(name: String) {
                 coffee2.remove(name)
                 this.bindingAdapter?.notifyDataSetChanged()
-
+                //this@KoffeeHolder.getBindingAdapter()
+                //super.getBindingAdapter()
             }
+
+
 
         }
 
--- app/src/main/java/com/example/recyclelist/MainActivity.kt
+++ app/src/main/java/com/example/recyclelist/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.recyclelist
 
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var kRecyclerView: RecyclerView
     val koffeeList = arrayListOf<String>("Arabic", "Robusta","Sumatra","Kona")
@@ -16,12 +25,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         kRecyclerView =
-            findViewById(R.id.coffee_recycler_view) as RecyclerView
+            findViewById<RecyclerView>(R.id.coffee_recycler_view)
         kRecyclerView.layoutManager = LinearLayoutManager(this)
 
         val kadapter: KAdapter  = KAdapter(this, koffeeList)
 
-        kRecyclerView.setAdapter(kadapter)
+        kRecyclerView.adapter = kadapter
+        // i.d of button = button
+        val clickButton: Button = findViewById(R.id.button)
+
+        clickButton.text = "Add"
+        clickButton.setOnClickListener(this)
+
+        val joinButton: Button = findViewById(R.id.button2)
+
+        joinButton.text = "Join"
+        joinButton.setOnClickListener(this)
+
 
     }
-}
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.button) {
+            var enteredText: String
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("Add")
+            alertDialog.setMessage("Name of Item to Add?")
+            val input: EditText = EditText(this)
+            input.setHint("Enter Text")
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            alertDialog.setPositiveButton("ok",
+                DialogInterface.OnClickListener { dialog, id ->
+                    enteredText = input.getText().toString()
+                    koffeeList.add(enteredText)
+                })
+            alertDialog.setView(input)
+            alertDialog.show()
+        }
+        else {
+            koffeeList.add(combindedList.toString())
+           for (i in combindedList) {koffeeList.remove(i) }
+            kRecyclerView.adapter?.notifyDataSetChanged()
+            combindedList = arrayListOf<String>()
+
+        }
+    }
+
+    }
