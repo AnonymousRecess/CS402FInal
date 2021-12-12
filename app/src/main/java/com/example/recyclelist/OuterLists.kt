c45895267903e5086d741d7998c3bd282d25210e
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2021-12-11 21:39:15 -0700

    dumb google sign in

--- app/src/main/java/com/example/recyclelist/OuterLists.kt
+++ app/src/main/java/com/example/recyclelist/OuterLists.kt
@@ -2,14 +2,15 @@ package com.example.recyclelist
 
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.View.inflate
 import android.view.Window
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -17,18 +18,61 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import java.util.*
 import java.util.zip.Inflater
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import androidx.annotation.NonNull
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+import com.google.android.gms.tasks.OnCompleteListener
+
+
+
+
+
+
 
 public class OuterLists : AppCompatActivity(), View.OnClickListener {
     private lateinit var outRecyclerView: RecyclerView
     private lateinit var alertDialog: AlertDialog
+    lateinit var mGoogleSignInClient: GoogleSignInClient
     val outerList = OuterModel() //TODO Create OuterModel class
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.outer_lists)
 
+        val imageView = findViewById<ImageView>(R.id.profileView)
+        val acctName = findViewById<TextView>(R.id.googleName)
+        val signOutOf = findViewById<ImageButton>(R.id.signOut)
+
+        signOutOf.setOnClickListener( object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                when(p0?.id) {
+                    R.id.signOut -> signOut()
+                    else -> null
+                }
+            }
+        })
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+
+            mGoogleSignInClient = login.mGoogleSignInClient
+            val personName = acct.displayName
+            val personGivenName = acct.givenName
+            val personFamilyName = acct.familyName
+            val personEmail = acct.email
+            val personId = acct.id
+            val personPhoto: Uri? = acct.photoUrl
+            Glide.with(this).load(personPhoto).into(imageView)
+            acctName.setText(personName)
+
+        }
         outRecyclerView =
             findViewById<RecyclerView>(R.id.outer_recycler_view)
         outRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -41,6 +85,7 @@ public class OuterLists : AppCompatActivity(), View.OnClickListener {
         
 
         outerAddButton.setOnClickListener(this)
+
         setTitle("Lists++")
     }
         override fun onClick(p0: View?) {
@@ -69,6 +114,15 @@ public class OuterLists : AppCompatActivity(), View.OnClickListener {
 
             dialog.dismiss()
         }
+    private fun signOut() {
+
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, OnCompleteListener<Void?> {
+                Toast.makeText(this,"Signed out", Toast.LENGTH_SHORT).show()
+                val signOutIntent: Intent = Intent(this, login::class.java)
+                startActivity(signOutIntent)
+            })
+    }
 
   
     
---
+++ app/src/main/java/com/example/recyclelist/login.kt
@@ -0,0 +1,87 @@
+package com.example.recyclelist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
+
+
+
+
+
+public class login: AppCompatActivity() {
+
+
+    private lateinit var signin: SignInButton
+    private var RC_SIGN_IN = 0;
+    companion object {
+        lateinit var mGoogleSignInClient: GoogleSignInClient
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_view)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+       signin =  findViewById(R.id.sign_in_button)
+        signin.setOnClickListener( object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                when(p0?.id) {
+                    R.id.sign_in_button -> signIn()
+                    else -> null
+                }
+            }
+        })
+    }
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+           val loggedInIntent: Intent = Intent(this, OuterLists::class.java)
+            startActivity(loggedInIntent)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("ERROR", "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account!=null)
+        {
+            val inIntent: Intent = Intent(this, OuterLists::class.java)
+            startActivity(inIntent)
+        }
+    }
+}
